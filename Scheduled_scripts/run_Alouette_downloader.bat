ab7362a8a6a6b2793b15629321010ff363ff9ba3
Naidoo <ravendra.naidoo@asc-csa.gc.ca>
2022-12-07 17:51:26 -0700

    update batch files

--- Scheduled_scripts/run_Alouette_downloader.bat
+++ Scheduled_scripts/run_Alouette_downloader.bat
@@ -1,2 +1,1 @@
-"C:\DevSoftware\Anaconda38\python.exe" "C:\Users\rnaidoo\Documents\Projects\Alouette_I\code\Alouette_extract\Scheduled_scripts\Alouette_downloader.py"
-pause
+"C:\DevSoftware\Anaconda38\python.exe" "C:\Users\rnaidoo\Documents\Projects\Alouette_I\code\Alouette_extract\Alouette_downloader.py"
---
+++ Scheduled_scripts/run_Alouette_processor_VDI.bat
@@ -0,0 +1,1 @@
+"C:\DevSoftware\Anaconda38\python.exe" "C:\Users\rnaidoo\Documents\Projects\Alouette_extract\Alouette_processor.py"
