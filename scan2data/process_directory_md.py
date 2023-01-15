aed97d788807ed835db4932ae5d1f1959355b31d
Naidoo <ravendra.naidoo@asc-csa.gc.ca>
2023-01-14 18:24:54 -0700

    Rav local

--- scan2data/process_directory_md.py
+++ scan2data/process_directory_md.py
@@ -165,7 +165,7 @@ def process_df_bottomside_metadata(df_processed, subdir_name, source_dir):
 def process_extract_management(dir_csv_output, master_dir, regex_raw, sample_subdir):
     
     df_processed, df_loss, df_outlier = process_subdirectory_md(sample_subdir, regex_raw)
-    df_processed = df_processed.dropna(subset=['dict_metadata'])
+    #df_processed = df_processed.dropna(subset=['dict_metadata'])
     
     # Split left from bottom-side metadata
     df_processed_left = df_processed.loc[df_processed['metadata_type'] == 'left']
