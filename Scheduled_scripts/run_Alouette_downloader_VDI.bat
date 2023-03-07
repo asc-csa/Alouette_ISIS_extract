0656d739efbd73fbdcfecb608e7cebdabeba7a6c
Naidoo <ravendra.naidoo@asc-csa.gc.ca>
2023-03-07 16:59:43 -0700

    Rav local

--- Scheduled_scripts/run_Alouette_downloader_VDI.bat
+++
@@ -1,1 +0,0 @@
-"C:\DevSoftware\Anaconda38\python.exe" "C:\Users\rnaidoo\Documents\Projects\Alouette_extract\Alouette_downloader.py" VDI
--- Scheduled_scripts/run_Alouette_processor.bat
+++
@@ -1,2 +0,0 @@
-"C:\DevSoftware\Anaconda38\python.exe" "C:\Users\rnaidoo\Documents\Projects\Alouette_I\code\Alouette_extract\Alouette_processor.py" local C:/Users/rnaidoo/Documents/Projects/Alouette_I/code/Alouette_extract/
-pause
