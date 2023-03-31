5edbf58f90c8825a91c1fe7557251ca91606ad7a
Naidoo <ravendra.naidoo@asc-csa.gc.ca>
2023-03-31 15:47:07 -0400

    Rav local

--- Alouette_processor.py
+++ Alouette_processor.py
@@ -19,9 +19,8 @@ process_on_VDI = True
 stop_loop_threshold = 2640 #max while loops to prevent infinite loop
 
 #Set directories
-rootDir_local = 'C:/Users/rnaidoo/Documents/Projects_data/Alouette_I/SuperVDI' + instance + '/BATCH_II_Run1/' #'U:/Data_Science/Projects_data/Alouette_I/SuperVDI' + instance + '/BATCH_I_Run1_rp/' 
-#rootDir_L_ = 'L:/DATA/Alouette_I/BATCH_I_Run1/'
-dataDir_L = 'L:/DATA/Alouette_I/BATCH_II_raw_ftp/'
+rootDir_local = 'G:/rnaidoo/Alouette_I/SuperVDI' + instance + '/BATCH_II_Run1/'
+dataDir_L = 'L:/DATA/Alouette_I/BATCH_II_raw/'
 rootDir_L = 'L:/DATA/Alouette_I/BATCH_II_Run1/'
 downloadedDir = rootDir_local + '02_downloaded/'
 processingDir = rootDir_local + '03_processing/'
