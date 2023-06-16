7978614411bc5bc60bf6bfdb670082438d4fc222
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2023-06-16 04:40:21 -0400

    Update scan_error_detection.py

--- error_detection/scan_error_detection.py
+++ error_detection/scan_error_detection.py
@@ -3,7 +3,7 @@
 # to kill this program: go ctrl+c
 
 # run these pip commands in anaconda prompt to download non-standard libraries (may need to add --user)
-# >pip install tensorflow==2.10
+# >pip install tensorflow (or for GPU use: >pip install tensorflow==2.10)
 # >pip install keras_ocr
 
 
