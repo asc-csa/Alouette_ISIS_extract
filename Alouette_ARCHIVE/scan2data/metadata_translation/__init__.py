79b42f11ba4ed7d7e4fc24aebaa195ab4d962219
Roksana <Roksana.Sheikholmolouki@gmail.com>
2024-09-09 12:19:56 -0400

    organization

--- Alouette_ARCHIVE/scan2data/metadata_translation/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Contains code to translate metadata located on the left
-"""
-
-import metadata_translation.leftside_metadata_grid_mapping
-import metadata_translation.translate_leftside_metadata
